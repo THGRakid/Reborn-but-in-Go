90c695af1816abfe467580687ec6b3f16e8d7610
Rakid <Kaiqing.Deng21@student.xjtlu.edu.cn>
2023-08-14 15:37:10 +0800

    message数据层搭建

--- Reborn-but-in-Go/config/init.go
+++ Reborn-but-in-Go/config/init.go
@@ -1,22 +1,31 @@
-package model
+package config
 
 import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"time"
 )
 
 // DB 数据库链接单例
 // 创建了一个名为 DB 的变量，用于在代码中保存一个指向 GORM v2 版本的数据库连接对象的指针
-var (
-	DB *gorm.DB
-)
+var DB *gorm.DB
 
 // 数据库连接的初始化。初始化函数会在程序启动时自动执行。
 func init() {
-	// 连接数据库
-	dsn := "Reborn_root:Go123456@tcp(sh-cynosdbmysql-grp-ni8vyxi2.sql.tencentcdb.com:24351)/reborn_but_in_go"
+
+	// MySQL 配置信息
+	const userName = "Reborn_root"                                 // 账号
+	const password = "Go123456"                                    // 密码
+	const host = "sh-cynosdbmysql-grp-ni8vyxi2.sql.tencentcdb.com" // 地址
+	const port = 24351                                             // 端口
+	const DBName = "reborn_but_in_go"                              // 数据库名称
+	const timeOut = "10s"                                          // 连接超时，10秒
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s", userName, password, host, port, DBName, timeOut)
+
+	// Open 连接
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info), // 设置日志级别
 	})
@@ -31,15 +40,14 @@ func init() {
 		fmt.Println("连接池初始化失败:", err)
 		return
 	}
-	//默认不加复数
-	sqlDB.SingularTable(true)
-	//设置连接池
+
 	//空闲
-	sqlDB.DB().SetMaxIdleConns(20)
+	sqlDB.SetMaxIdleConns(20)
 	//打开
-	sqlDB.DB().SetMaxOpenConns(100)
+	sqlDB.SetMaxOpenConns(100)
 	//超时
-	sqlDB.DB().SetConnMaxLifetime(time.Second * 30)
+	sqlDB.SetConnMaxLifetime(time.Second * 30)
+
 	DB = db // 将数据库连接保存到全局变量
 }
 
---
+++ Reborn-but-in-Go/go.mod
@@ -0,0 +1,10 @@
+module Reborn-but-in-Go
+
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	gorm.io/driver/mysql v1.5.1
+	gorm.io/gorm v1.25.2
+)
