81604933e77d14e7787fa02b5d303f263668208e
Rakid <Kaiqing.Deng21@student.xjtlu.edu.cn>
2023-08-17 11:18:39 +0800

    config配置相关逻辑，simple-demo为原始提供接口样例，现更改名字方便辨别,最终项目不会带这个包

--- Reborn-but-in-Go/main.go
+++ Reborn-but-in-Go/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"Reborn-but-in-Go/config"
+	"github.com/THGRakid/Reborn-but-in-Go/config"
+	"net/http"
+)
 
+func main() {
+	r := config.GetGinEngine()
+
+	prefix := "/api"
+
+	r.GET(prefix+"/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello from API!"})
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
