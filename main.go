d36e77d27eb44865c2a0bf6765e4854120bf8733
Rakid <Kaiqing.Deng21@student.xjtlu.edu.cn>
2023-08-17 11:42:46 +0800

    所以项目更改路径，取消二级目录

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"Reborn-but-in-Go/config"
-	"github.com/THGRakid/Reborn-but-in-Go/config"
+	"github.com/gin-gonic/gin"
 	"net/http"
+	"字节青训营/Reborn-but-in-Go/config"
 )
 
 func main() {
