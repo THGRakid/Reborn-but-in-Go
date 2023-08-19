1fc3cf476382218121c5bf2e5db9bb892535631d
Rakid <Kaiqing.Deng21@student.xjtlu.edu.cn>
2023-08-19 13:47:56 +0800

    message模块完整简单实现（附带文件说明）

--- message/main.go
+++ message/main.go
@@ -22,8 +22,8 @@ func main() {
 	// 注册 GET 路由，处理获取聊天消息的请求，使用表现层中的 QueryMessageList 函数
 	r.GET("/douyin/message/chat", messageController.QueryMessage)
 
-	// 注册 POST 路由，处理发送消息操作的请求，使用表现层中的 MessageActionHandler 函数
-	r.POST("/douyin/message/action", messageController.MessageActionHandler)
+	// 注册 POST 路由，处理发送消息操作的请求，使用表现层中的 SendMessage 函数
+	r.POST("/douyin/message/action", messageController.SendMessage)
 
 	// 启动服务器并监听在 :8080 端口上
 	if err := r.Run(":8080"); err != nil {
