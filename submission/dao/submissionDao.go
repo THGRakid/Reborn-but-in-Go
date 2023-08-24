5b6abfdb9ae0782390666722935f088d79d5ca79
happy <2630391116@qq.com>
2023-08-25 00:04:43 +0800

    submission，Dao层代码完成

--- submission/dao/submissionDao.go
+++ submission/dao/submissionDao.go
@@ -1,57 +1,54 @@
 package dao
 
 import (
-	"errors"
-	"log"
-	"time"
+	"Reborn-but-in-Go/config"
+	"Reborn-but-in-Go/submission/model"
+	"fmt"
+	"sync"
 )
 
-type Video struct {
-	Id            int64     //视频id
-	UserId        int64     //作者id
-	VideoPath     string    //视频地址
-	CoverPath     string    //视频封面地址
-	FavoriteCount int64     //点赞数
-	CommentCount  int64     //评论数
-	Title         string    //视频标题
-	Time          time.Time //投稿时间
-	Status        int32     //待审核0 发布成功1 审核失败2 下架删除3
+/*
+	？？？？构造方法？？？？
+*/
+
+type VideoDao struct {
 }
 
-func (Video) TableName() string {
-	return "videos"
+// 用于保存单例实例
+var videoDao *VideoDao
+
+// 单例模式，只生成一个VideoDao实例，提高性能
+var videoOnce sync.Once
+
+// NewVideoDaoInstance 用于获取VideoDao单例实例的函数
+// 传递一个匿名函数（闭包），其作用是创建一个新的 MessageDao 实例并将其赋值给 messageDao 变量。
+// 这个函数只会在第一次调用 Do 方法时执行
+func NewVideoDaoInstance() *VideoDao {
+	videoOnce.Do(
+		func() {
+			videoDao = &VideoDao{}
+		})
+	return videoDao
 }
 
-// 1、 视频发布
-func InsertVideo(video Video) (Video, error) {
-	log.Println("SubmissionDao-InsertVideo: running")
-	//往数据库中插入一条 视频 信息
-	err := db.Model(Video{}).Create(&video)
-	if err != nil {
-		log.Println("SubmissionDao-InsertVideo: return create video failed") //打印错误信息
-		return Video{}, errors.New("create video failed")
-	}
-	log.Println("SubmissionDao-InsertVideo: return success")
-	return video, nil
-}
-
-// 2、 视频投稿列表
-func GetSubmittedVideoList(userId int64) ([]Video, error) {
-	// 函数已运行
-	log.Println("SubmissionDao-GetSubmittedVideoList: running")
-	var videoList []Video
-	//在数据库中查询
-	result := db.Model(Video{}).Where(map[string]interface{}{"user_id": userId}).
-		Order("time desc").Find(&videoList)
-	//若该用户没有投稿视频，返回空列表，不报错
-	if result.RowAffected == 0 {
-
-		return nil, nil
-	}
-	//获取投稿视频出错
+// 1、视频发布。创建一个新的video。
+func (*VideoDao) CreateVideo(video *model.Video) error {
+	//将video内数据导入数据库
+	result := config.DB.Create(&video)
 	if result.Error != nil {
-		log.Println(result.Error.Error())
-		return videoList, errors.New("get submitted video list failed")
+		return result.Error
 	}
-	return videoList, nil
+	return nil
+}
+
+// 2、视频列表。根据userID，查出video列表
+func (*VideoDao) QueryVideoList(userId int64) ([]*model.Video, error) {
+	var videos []*model.Video
+	//根据user_id查询视频列表，按从大到小排序
+	err := config.DB.Where("user_id = ?", userId).Order("time desc").Find(&videos).Error
+	if err != nil {
+		fmt.Println("Failed to get video list")
+		return nil, err
+	}
+	return videos, nil
 }
