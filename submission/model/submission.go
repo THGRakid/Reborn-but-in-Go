98ea9c6a8ccb866d1fe00a84e6f2a466797845de
happy <2630391116@qq.com>
2023-08-24 18:35:13 +0800

    submission-model，持久层结构块 以及 修改表名映射

---
+++ submission/model/submission.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+)
+
+// Submission 持久层结构块
+type Video struct {
+	Id            int64     `gorm:"primaryKey"` //视频id
+	UserId        int64     //作者id
+	VideoPath     string    //视频地址
+	CoverPath     string    //视频封面地址
+	FavoriteCount int64     //点赞数
+	CommentCount  int64     //评论数
+	Title         string    //视频标题
+	Time          time.Time //投稿时间
+	Status        int32     //待审核0 发布成功1 审核失败2 下架删除3
+}
+
+// TableName 修改表名映射
+func (Video) TableName() string {
+	return "videos"
+}
