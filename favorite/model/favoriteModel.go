e6b3e50384e7b9ed81a63820394711784150fd8f
hao.dev <1194924237@qq.com>
2023-08-26 15:12:50 +0800

    favorite模块dao层更新提交

--- favorite/model/favoriteModel.go
+++ favorite/model/favoriteModel.go
@@ -4,10 +4,10 @@ import "time"
 
 // Favorite 持久层结构块
 type Favorite struct {
-	UserId    int64
-	VideoId   int64
-	CreatedAt time.Time //点赞时间
-	Status    int8      //点赞状态（点赞为1，取消赞为2）
+	UserId  int64
+	VideoId int64
+	Time    time.Time //点赞时间
+	Status  int8      //点赞状态（点赞为1，取消赞为2）
 }
 
 // TableName 修改表名映射
