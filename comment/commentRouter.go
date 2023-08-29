c12e6e3feaf91373a202e2b2da96f8c933735789
Yfanx <2236388009@qq.com>
2023-08-29 16:35:32 +0800

    修正了注释

--- comment/commentRouter.go
+++ comment/commentRouter.go
@@ -17,10 +17,10 @@ func InitCommentRouter(r *gin.Engine) {
 	// 创建表现层（Controller）的实例，传递服务层实例
 	commentController := controller.NewCommentController(commentService)
 
-	// 注册 GET 路由，处理获取聊天消息的请求，使用表现层中的 QueryMessageList 函数
+	// 注册 GET 路由，查看视频的所有评论，按发布时间倒序，使用表现层中的 GetCommentList 函数
 	r.GET("/douyin/comment/list/", commentController.GetCommentList)
 
-	// 注册 POST 路由，处理发送消息操作的请求，使用表现层中的 SendMessage 函数
+	// 注册 POST 路由，登录用户对视频进行评论，使用表现层中的 HandleCommentAction 函数
 	r.POST("/douyin/comment/action/", commentController.HandleCommentAction)
 
 }
