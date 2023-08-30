9d99373dfdc8079e194d6533e09095008ed71446
Mr-weekend <2096881337@qq.com>
2023-08-30 19:25:56 +0800

    中间件加入创建token和校验token

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -11,7 +11,45 @@ AuthMiddleware中间件
 import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"time"
 )
+
+var Key = []byte("Reborn_But_In_Go") //加密key
+type MyClaims struct {
+	UserId   int64  `json:"user_id"`
+	UserName string `json:"username"`
+	jwt.StandardClaims
+}
+
+// CreateToken 生成一个token
+func CreateToken(userId int64, userName string) (string, error) {
+	expireTime := time.Now().Add(24 * time.Hour) //过期时间
+	nowTime := time.Now()                        //当前时间
+	claims := MyClaims{
+		UserId:   userId,
+		UserName: userName,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(), //过期时间戳
+			IssuedAt:  nowTime.Unix(),    //当前时间戳
+			Issuer:    "zhoumo",          //颁发者签名
+			Subject:   "userToken",       //签名主题
+		},
+	}
+	tokenStruct := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return tokenStruct.SignedString(Key)
+}
+
+// CheckToken 验证token
+func CheckToken(token string) (*MyClaims, bool) {
+	tokenObj, _ := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return Key, nil
+	})
+	if key, _ := tokenObj.Claims.(*MyClaims); tokenObj.Valid {
+		return key, true
+	} else {
+		return nil, false
+	}
+}
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
