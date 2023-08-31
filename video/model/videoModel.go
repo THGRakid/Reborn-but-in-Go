4d72745b23c73740192c87b8d8abc83ba3756a44
Mr-weekend <2096881337@qq.com>
2023-09-01 01:59:08 +0800

    加了JSON信息

--- video/model/videoModel.go
+++ video/model/videoModel.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type Video struct {
-	Id            int64     //视频id
-	UserId        int64     //作者id
-	VideoPath     string    //视频地址
-	CoverPath     string    //视频封面地址
-	FavoriteCount int64     //点赞数
-	CommentCount  int64     //评论数
-	Title         string    //视频标题
+	Id            int64     `json:"id,omitempty"`   //视频id
+	UserId        int64     `json:"author"`         //作者id
+	VideoPath     string    `json:"play_url"`       //视频地址
+	CoverPath     string    `json:"cover_url"`      //视频封面地址
+	FavoriteCount int64     `json:"favorite_count"` //点赞数
+	CommentCount  int64     `json:"comment_count"`  //评论数
+	Title         string    `json:"title"`          //视频标题
 	CreateAt      time.Time //投稿时间
 	Status        int32     //待审核0 发布成功1 审核失败2 下架删除3
 }
